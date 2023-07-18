89c2192bf2c55ed260a12bf013d3317b785050c8
Vrund <vrundshah7490@gmail.com>
2023-07-18 10:41:27 +0530

    feat: 🎸 Typography added

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant='h1'>h1 Heading</Typography>
+      <Typography variant='h2'>h2 Heading</Typography>
+      <Typography variant='h3'>h3 Heading</Typography>
+      <Typography variant='h4' gutterBottom>h4 Heading</Typography>
+      <Typography variant='h5'>h5 Heading</Typography>
+      <Typography variant='h6'>h6 Heading</Typography>
+      <Typography variant='subtitle1'>SubTitle 1</Typography>
+      <Typography variant='subtitle2'>SubTitle 2</Typography>
+      <Typography variant='body1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, ullam tempora in distinctio voluptates nulla saepe! Veritatis inventore, unde totam quod, voluptatem omnis atque, ut sunt ipsum perspiciatis nostrum repellendus.</Typography>
+      <Typography variant='body2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, deserunt reprehenderit enim quas ad officiis maiores cum dolorum incidunt! Natus quis veritatis velit temporibus eius sunt consequatur quo dolor eos.</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
