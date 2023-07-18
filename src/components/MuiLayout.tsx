6e54218e1ff771f0a257ffa4a0a04d86fd66f331
Vrund <vrundshah7490@gmail.com>
2023-07-18 22:23:01 +0530

    feat: 🎸 Slack added

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,13 +1,13 @@
-import { Box } from '@mui/material'
+import { Box, Stack, Divider } from '@mui/material'
 
 const MuiLayout = () => {
   return (
-    <>
+    <Stack sx={{ border: '1px solid'}} direction="row" spacing={2} divider={<Divider orientation='vertical' flexItem />}>
 
       <Box sx={{
         backgroundColor: 'primary.main',
         color: 'white',
-        height: '20px',
+        height: '100px',
         width: '100px',
         padding: '16px',
         '&:hover': {
@@ -17,7 +17,7 @@ const MuiLayout = () => {
       <Box display="flex" height="100px" width="100px" bgcolor="success.light" p={2}>
 
       </Box>
-    </>
+    </Stack>
   )
 }
 
